60e21ceb95964215607dc2dae58f0d2effaf12aa
Reon0429-cat <oonishireon110@gmail.com>
2021-12-04 16:53:45 +0900

    text練習

--- app/src/main/java/com/example/kotlin_practice01/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice01/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // activity_main.xmlで指定したものを読み込んでいる
+        //    R.layout.activity_main
+        // -> res/layout/activity_main.xml
         setContentView(R.layout.activity_main)
     }
 }
