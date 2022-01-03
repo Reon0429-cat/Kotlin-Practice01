0e58ab5e56df8c2a35f3bd32b58db450e99ffcf5
Reon0429-cat <oonishireon110@gmail.com>
2022-01-03 22:30:46 +0900

    スコープ関数練習

--- app/src/main/java/com/example/kotlin_practice01/ConstructorSample.kt
+++ app/src/main/java/com/example/kotlin_practice01/ConstructorSample.kt
@@ -1,32 +1,18 @@
 package com.example.kotlin_practice01
 
 import android.util.Log
+import java.util.*
 import android.util.Log.d as d1
 
 class ConstructorSample {
 
 }
 
-open class Person(val name: String) {}
-class Student(name: String, var degree: String): Person(name) {}
-val student = Student("REON", "学生")
 
 
-interface Animal {
-    fun eat()
-    fun say() {
-        print("hello")
-    }
-}
-
-class Dog: Animal {
-    override fun eat() {
-        print("ドッグフード")
-    }
-    override fun say() {
-        super.say()
-
-        Log.d("REON", "私は犬です")
-
+class Person(val name: String) {
+    var age: Int = 0
+    fun message() {
+        Log.d("message", "私は $name です。 $age 歳です。")
     }
 }
--- app/src/main/java/com/example/kotlin_practice01/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice01/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.kotlin_practice01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.kotlin_practice01.databinding.ActivityMainBinding
-
-
-
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,13 +23,46 @@ class MainActivity : AppCompatActivity() {
             binding.textView.text = "ボタンが押されました"
         }
 
-        // activity_main.xmlで指定したものを読み込んでいる
-        //    R.layout.activity_main
-        // -> res/layout/activity_main.xml
-//        setContentView(R.layout.activity_main)
 
-        val dog = Dog()
-        dog.say()
+//        val person = Person("REON")
+//        person.age = 21
+//        Log.d("Person", person.message()
+        // 私は REON です。 21 歳です。
+
+//        with(Person("REON")) {
+//            age = 21
+//            message()
+//        }
+//
+//        Person("REON").apply {
+//            age = 22
+//        }.message()
+//
+//        var name: String? = "reon"
+//        var upperCaseName = name?.uppercase(Locale.getDefault())
+//        var nameLength = name?.length
+//        Log.d("message", "$upperCaseName $nameLength") // REON 4
+
+        var name: String? = "reon"
+//        var output = name?.let {
+//            var upperCaseName = it.uppercase(Locale.getDefault())
+//            var nameLength = it.length
+//            "$upperCaseName $nameLength"
+//        } ?: "null"
+//        Log.d("message", output)
+
+        var output = name?.run {
+            "${uppercase(Locale.getDefault())} $length"
+        } ?: "null"
+        Log.d("message", output)
+
+
+        val arrayInt = listOf(1, 2, 3).also {
+            Log.d("message", "$it") // [1, 2, 3]
+        }
+        for (int in arrayInt) {
+            Log.d("message", "$int")
+        }
 
     }
 
